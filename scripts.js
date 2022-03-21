5837d75bbe5bd820a5848a0c87783999edd3492d
alemenfon <alemenfon1@gmail.com>
2022-03-21 19:21:28 +0100

    Subida prueba

---
+++ scripts.js
@@ -0,0 +1,70 @@
+let numero=-4;
+let palabra ='Hola mundo';
+let respuesta=true;
+const PI=3.14;
+numero=54;
+console.log(numero);
+console.log(palabra);
+console.log(respuesta);
+console.log(PI);
+
+//Ejercicio 2
+ let num=5;
+ let num2=2;
+ let nombre='ALE';
+
+ console.log(num+num2);
+ console.log(num-num2);
+ console.log(num*num2);
+ console.log(num/num2);
+ console.log(num%num2);
+ console.log(num-num2);
+ console.log(nombre);
+ console.log('Hola '+nombre);
+
+ //Ejercicio 3
+
+ let a=5;
+ console.log(a);
+ console.log(a++);
+ console.log(a);
+ console.log(++a); // suma la variable y luego la muestra
+
+
+ console.log(a);
+ console.log(a--);
+ console.log(a);
+ console.log(--a)
+ console.log(a+=3);
+ console.log(a-=3);
+
+ //Ejercicio cadenas
+
+ let cadena="Hola mundo";
+ console.log(cadena.length);
+ console.log(cadena.toUpperCase(cadena));
+ console.log(cadena.toLowerCase(cadena));
+ console.log(cadena.indexOf("o"));
+ console.log(cadena.indexOf("Hola"));
+ console.log(cadena.replace("Mundo","Youtube")); 
+ console.log(cadena.substring(cadena.length/2));
+ console.log(cadena.startsWith("h"));
+ console.log(cadena.startsWith("H"));
+ console.log("r".repeat(10));
+
+ let nomb="Alejandra";
+ let apellido="Mensaque";
+ let edad="3";
+
+ console.log(`Me llamo	${nomb}	${apellido}. Tengo	${edad});
+ 
+
+
+ 
+
+
+
+
+
+
+
